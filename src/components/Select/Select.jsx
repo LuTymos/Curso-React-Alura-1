fd250f596934769853b468324773314e517b410b
LucasTymos <lucastymos@gmail.com>
2023-12-20 22:40:28 -0300

    fix: arrumando bug de não limpar o select e também a importação de imagens

--- src/components/Select/Select.jsx
+++ src/components/Select/Select.jsx
@@ -15,6 +15,7 @@ function Select({ Id, label, options, required, valueInput, setValueInput }) {
         required={required}
         id={Id}
       >
+        <option value=""> Selecione uma opção</option>
         {options.map((option, index) => (
           <option key={index} value={option.value}>
             {option.label}
