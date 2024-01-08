e613e39d491921ddd63f65e6117f570af71bb5db
LucasTymos <lucastymos@gmail.com>
2024-01-07 22:22:29 -0300

    Finalização projeto

--- src/components/Colaborador/index.jsx
+++ src/components/Colaborador/index.jsx
@@ -1,14 +1,49 @@
 import "./index.css";
+import { AiFillCloseCircle, AiFillHeart, AiOutlineHeart } from "react-icons/ai";
 
-const Colaborador = ({ nome, cargo, imagem, primaryColor }) => {
+const Colaborador = ({
+  nome,
+  cargo,
+  imagem,
+  primaryColor,
+  aoDeletar,
+  id,
+  favorito,
+  aoFavoritar,
+}) => {
   return (
     <div className="colaborador">
+      <AiFillCloseCircle
+        size={25}
+        className="deletar"
+        onClick={() => {
+          aoDeletar(id);
+        }}
+      />
       <div className="cabecalho" style={{ backgroundColor: primaryColor }}>
         <img src={imagem} alt={nome} />
       </div>
       <div className="rodape">
         <h4>{nome}</h4>
         <h5>{cargo}</h5>
+        <div className="favoritar">
+          {favorito ? (
+            <AiFillHeart
+              size={25}
+              color="#ff0000"
+              onClick={() => {
+                aoFavoritar(id);
+              }}
+            />
+          ) : (
+            <AiOutlineHeart
+              size={25}
+              onClick={() => {
+                aoFavoritar(id);
+              }}
+            />
+          )}
+        </div>
       </div>
     </div>
   );
--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -3,21 +3,41 @@ import Input from "../input/Input";
 import Select from "../Select/Select";
 import Button from "../Button/Button";
 import { useState } from "react";
+import { v4 as uuidv4 } from "uuid";
 
-function Formulario({ cadastroColaborador, times }) {
+function Formulario({ cadastroColaborador, times, cadastrarTime }) {
   const [nome, setNome] = useState();
   const [cargo, setCargo] = useState();
   const [imagem, setImage] = useState();
   const [time, setTime] = useState();
 
+  const [nomeTime, setNomeTime] = useState();
+  const [corTime, setCorTime] = useState();
+
   const submitForm = (e) => {
     e.preventDefault();
-    cadastroColaborador({ nome, cargo, imagem, time });
+    cadastroColaborador({
+      nome,
+      cargo,
+      imagem,
+      time,
+      id: uuidv4(),
+      favorito: false,
+    });
     setCargo("");
     setNome("");
     setImage("");
     setTime("");
   };
+  function formatarString(str) {
+    // Converter para minúsculas
+    let strMinuscula = str.toLowerCase();
+
+    // Substituir espaços por underscores
+    let strFormatada = strMinuscula.replace(/ /g, "_");
+
+    return strFormatada;
+  }
 
   return (
     <section className="formulario">
@@ -63,6 +83,38 @@ function Formulario({ cadastroColaborador, times }) {
         />
 
         <Button>Criar card</Button>
+      </form>
+
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          cadastrarTime({
+            label: nomeTime,
+            cor: corTime,
+            value: formatarString(nomeTime),
+          });
+        }}
+      >
+        <h2>Preencha os dados para criar um time</h2>
+        <Input
+          required
+          type="text"
+          label="Nome do time"
+          placeholder="Digite o nome do time"
+          Id="nomeTime"
+          valueInput={nomeTime}
+          setValueInput={(valueInput) => setNomeTime(valueInput)}
+        />
+        <Input
+          required
+          type="color"
+          label="Cor do time"
+          placeholder="Digite a cor do time"
+          Id="corTime"
+          valueInput={corTime}
+          setValueInput={(valueInput) => setCorTime(valueInput)}
+        />
+        <Button>Criar um novo time</Button>
       </form>
     </section>
   );
